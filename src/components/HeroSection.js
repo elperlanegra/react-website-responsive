dfa13add7d7e37d53cfb18b1c464fc5ba11dc578
Manuel <carlosd99m@gmail.com>
2021-02-19 21:38:22 -0600

    Pagina home

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../App.css';
-import { Button } from '/Button';
+import { Button } from './Button';
 import './HeroSection.css';
 
 function HeroSection() {
@@ -10,8 +10,8 @@ function HeroSection() {
             <h1>ADVENTURE AWAITS</h1>
             <p>What are you waiting for?</p>
             <div className="hero-btns">
-                <Button className="btns" buttonStyle='button--primary' buttonSize='btn--large'>GET STARTED</Button>
-                <Button className="btns" buttonStyle='button--outline' buttonSize='btn--large'>
+                <Button className="btns" buttonStyle='button--outline' buttonSize='btn--large'>GET STARTED</Button>
+                <Button className="btns" buttonStyle='button--primary' buttonSize='btn--large'>
                     WATCH TRAILER
                     <i className='far fa-play-circle'/>
                 </Button>
